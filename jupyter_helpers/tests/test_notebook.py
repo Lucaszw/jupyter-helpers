d405ec80de359a20bef6b9b1f79a2038c88d53c3
lucaszw <lucas.zeer@gmail.com>
2017-06-02 16:01:53 +0800

    stopping thread death

--- jupyter_helpers/tests/test_notebook.py
+++ jupyter_helpers/tests/test_notebook.py
@@ -7,8 +7,6 @@ from jupyter_helpers import notebook
 def test_get_session():
     sm = notebook.SessionManager()
     session = sm.get_session()
-    session.thread.stop()
-    session.thread.join()
     os.kill(session.process.pid, signal.CTRL_C_EVENT)
     session.process.kill()
     pass
