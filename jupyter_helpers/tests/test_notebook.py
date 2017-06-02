3ea04b322945929e9cc6dd4f903a986aad91646b
lucaszw <lucas.zeer@gmail.com>
2017-06-02 14:25:03 +0800

    added kill process

--- jupyter_helpers/tests/test_notebook.py
+++ jupyter_helpers/tests/test_notebook.py
@@ -1,10 +1,13 @@
+import os
+import signal
 import sys
 
 from jupyter_helpers import notebook
 
 def test_get_session():
-    sm = notebook.SessionManager(True)
+    sm = notebook.SessionManager(False)
     session = sm.get_session()
     session.thread.stop()
     session.thread.join()
+    os.kill(session.process.pid, signal.CTRL_C_EVENT)
     pass
