c3acde6b8b83ed4d2a5e9ee80ce4b77371829bce
lucaszw <lucas.zeer@gmail.com>
2017-06-02 16:23:54 +0800

    testing with daemon false

--- jupyter_helpers/tests/test_notebook.py
+++ jupyter_helpers/tests/test_notebook.py
@@ -5,8 +5,9 @@ import sys
 from jupyter_helpers import notebook
 
 def test_get_session():
-    sm = notebook.SessionManager()
+    sm = notebook.SessionManager(False)
     session = sm.get_session()
+    session.thread.stop()
+    session.thread.join()
     os.kill(session.process.pid, signal.CTRL_C_EVENT)
-    session.process.kill()
     pass
