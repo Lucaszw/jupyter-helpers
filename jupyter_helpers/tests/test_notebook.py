1e7c36ef547c7353713b7367cde845b40bdd7de9
lucaszw <lucas.zeer@gmail.com>
2017-06-02 14:33:13 +0800

    cleaned up test

--- jupyter_helpers/tests/test_notebook.py
+++ jupyter_helpers/tests/test_notebook.py
@@ -5,9 +5,8 @@ import sys
 from jupyter_helpers import notebook
 
 def test_get_session():
-    sm = notebook.SessionManager(False)
+    sm = notebook.SessionManager()
     session = sm.get_session()
     session.thread.stop()
-    session.thread.join()
     os.kill(session.process.pid, signal.CTRL_C_EVENT)
     pass
