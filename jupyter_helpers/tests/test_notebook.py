a364f36c0f5ae668673fb7d905641e5134f2ef0a
lucaszw <lucas.zeer@gmail.com>
2017-06-02 10:20:25 +0800

    testing without launching notebook

--- jupyter_helpers/tests/test_notebook.py
+++ jupyter_helpers/tests/test_notebook.py
@@ -1,7 +1,7 @@
 from jupyter_helpers import notebook
 
 def test_get_session():
-    sm = notebook.SessionManager()
-    sm.get_session()
-    sm.stop()
-    assert False
+    #sm = notebook.SessionManager()
+    #sm.get_session()
+    #sm.stop()
+    pass
