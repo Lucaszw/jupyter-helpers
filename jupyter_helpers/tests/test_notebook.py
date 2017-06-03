27f38006edcf00e1d7dac66ecaa47c48a2a4d0eb
lucaszw <lucas.zeer@gmail.com>
2017-06-03 15:38:10 +0800

    testing terminating processs with psutil

--- jupyter_helpers/tests/test_notebook.py
+++ jupyter_helpers/tests/test_notebook.py
@@ -1,13 +1,19 @@
 import os
 import signal
+import subprocess
 import sys
+
+import psutil
 
 from jupyter_helpers import notebook
 
 def test_get_session():
     sm = notebook.SessionManager(False)
     session = sm.get_session()
-    session.thread.stop()
-    session.thread.join()
-    os.kill(session.process.pid, signal.CTRL_C_EVENT)
+    # session.thread.stop()
+    process = psutil.Process(session.process.pid)
+    for proc in process.children(recursive=True):
+        proc.kill()
+    process.kill()
+    #os.kill(session.process.pid, signal.CTRL_C_EVENT)
     pass
